805f224ebb9531d6db65a95842e1886c4219416e
Fran Moreno <fran_99_cbm@hotmail.com>
2021-02-24 09:01:16 +0100

    New Dataset added: MyDataset2
    It contains all the frames from a video in CAUDHE
    dataset.

--- Project/DeepHomography/OnelineDLTv1/dataset.py
+++ Project/DeepHomography/OnelineDLTv1/dataset.py
@@ -240,6 +240,7 @@ class MyDataset(Dataset):
         HEIGHT = org_img.shape[1] # org_img.shape[0] = 1 (channel)
 
         x, y = 0, 0
+        # By using patch = complete image -> input_tensor == org_img
         input_tensor = org_img[:, y: y+self.patch_h, x: x+self.patch_w]
 
         y_t_flat = np.reshape(self.y_mesh, [-1])
@@ -256,6 +257,94 @@ class MyDataset(Dataset):
 
         return (org_img, input_tensor, patch_indices, four_points,print_img_1,
                 print_img_2, video_name, npy_id)
+
+    def __len__(self):
+        return len(self.pair_list)
+
+
+class MyDataset2(Dataset):
+    def __init__(self, data_path, patch_w=560, patch_h=315, rho=16, WIDTH=640, HEIGHT=360):
+        self.mean_I = np.reshape(np.array([118.93, 113.97, 102.60]), (1, 1, 3))
+        self.std_I = np.reshape(np.array([69.85, 68.81, 72.45]), (1, 1, 3))
+
+        self.patch_h = patch_h
+        self.patch_w = patch_w
+        self.WIDTH = WIDTH
+        self.HEIGHT = HEIGHT
+        self.rho = rho
+        self.x_mesh, self.y_mesh = make_mesh(self.patch_w, self.patch_h)
+
+        self.work_dir = os.path.join(data_path, 'Data')
+        self.pair_list = list(open(os.path.join(self.work_dir, 'Test_List.txt')))
+        self.pair_list = [self.pair_list[i] for i in range(len(self.pair_list)) if self.pair_list[i][:7] == '0000016']
+        print(len(self.pair_list))
+        self.img_path = os.path.join(self.work_dir, 'MyTest2/')
+        self.npy_path = os.path.join(self.work_dir, 'Coordinate/')
+
+    def __getitem__(self, index):
+
+        img_pair = self.pair_list[index]
+        pari_id = img_pair.split(' ')
+
+        # Not used by now #
+        npy_id = pari_id[0].split('/')[1] + '_' + pari_id[1].split('/')[1][:-1] + '.npy'
+        npy_id = self.npy_path + npy_id
+        video_name = img_pair.split('/')[0]
+
+        # load img1
+        if pari_id[0][-1] == 'M':
+            img_1 = cv2.imread(self.img_path + pari_id[0][:-2])
+        else:
+            img_1 = cv2.imread(self.img_path + pari_id[0])
+
+        # load img2
+        if pari_id[1][-2] == 'M':
+            img_2 = cv2.imread(self.img_path + pari_id[1][:-3])
+        else:
+            img_2 = cv2.imread(self.img_path + pari_id[1][:-1])
+
+        height, width = img_1.shape[:2]
+
+        if height != self.HEIGHT or width != self.WIDTH:
+            img_1 = cv2.resize(img_1, (self.WIDTH, self.HEIGHT))
+
+        print_img_1 = img_1.copy()
+        print_img_1 = np.transpose(print_img_1, [2, 0, 1])
+
+        img_1 = (img_1 - self.mean_I) / self.std_I
+        img_1 = np.mean(img_1, axis=2, keepdims=True)
+        img_1 = np.transpose(img_1, [2, 0, 1])
+
+        height, width = img_2.shape[:2]
+
+        if height != self.HEIGHT or width != self.WIDTH:
+            img_2 = cv2.resize(img_2, (self.WIDTH, self.HEIGHT))
+
+        print_img_2 = img_2.copy()
+        print_img_2 = np.transpose(print_img_2, [2, 0, 1])
+        img_2 = (img_2 - self.mean_I) / self.std_I
+        img_2 = np.mean(img_2, axis=2, keepdims=True)
+        img_2 = np.transpose(img_2, [2, 0, 1])
+        org_img = np.concatenate([img_1, img_2], axis=0)
+        WIDTH = org_img.shape[2]
+        HEIGHT = org_img.shape[1]
+
+        x, y = 0, 0
+        input_tesnor = org_img[:, y: y + self.patch_h, x: x + self.patch_w]
+
+        y_t_flat = np.reshape(self.y_mesh, [-1])
+        x_t_flat = np.reshape(self.x_mesh, [-1])
+        patch_indices = (y_t_flat + y) * WIDTH + (x_t_flat + x)
+
+        top_left_point = (x, y)
+        bottom_left_point = (x, y + self.patch_h)
+        bottom_right_point = (self.patch_w + x, self.patch_h + y)
+        top_right_point = (x + self.patch_w, y)
+        four_points = [top_left_point, bottom_left_point, bottom_right_point, top_right_point]
+
+        four_points = np.reshape(four_points, (-1))
+
+        return (org_img, input_tesnor, patch_indices, four_points, print_img_1, print_img_2, video_name, npy_id)
 
     def __len__(self):
         return len(self.pair_list)
